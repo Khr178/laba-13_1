de1a3045be93a85bf844ff7b55a7a1d309b802b6
Khrystyna <hrustunakopeika2005@gmail.com>
2022-12-02 17:37:20 +0200

    first master commit

---
+++ лаба 13.1.cpp
@@ -0,0 +1,46 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+// Lab_13_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// головний файл проекту – функція main
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+#include "dod.h"
+#include "sum.h"
+#include "var.h"
+
+using namespace std;
+
+using namespace nsDod;
+using namespace nsSum;
+using namespace nsVar;
+int main()
+{
+
+	cout << "x_p = "; cin >> x_p;
+	cout << "x_k = "; cin >> x_k;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << endl;
+
+	x = x_p;
+	if (x > 1)
+		while (x <= x_k) {
+			sum(); // виклик процедури обчислення суми
+			cout << x << " "
+				<< s << " "
+				<< atan(x) << " "
+				<< n << endl; // тут слід використовувати форматний вивід
+			x += dx;
+		}
+	cin.get();
+	return 0;
+}
+
+
+
+
+
+	
---
+++ юніт тест 13.1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаба 13.1/var.h"
+#include "../лаба 13.1/var.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест131
+{
+	TEST_CLASS(юніттест131)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			nsVar::x_k = 2;
+			nsVar::x_p = 3;
+			double expected = 0;
+			Assert::AreEqual(nsVar::a, expected);
+		}
+	};
+}
